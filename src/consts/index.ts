25b4478ba4b216b5dd510919451669e917514ce7
alaltru1992 <33177697+alaltru1992@users.noreply.github.com>
2025-02-26 19:09:17 +0300

    markup 2 + fetch

---
+++ src/consts/index.ts
@@ -0,0 +1,2 @@
+export const COCKTAIL_NAMES = [{name: 'Margarita', code: 'margarita'}, {name: 'Mojito', code: 'mojito'},
+    {name: 'A1', code: 'a1'}, {name: 'Kir', code: 'kir'}]
--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,6 +1,35 @@
-import classes from './styles.module.scss'
+import {useState} from "react";
+import classes from './styles.module.scss';
+import {COCKTAIL_NAMES} from "consts";
 
 export default () =>{
+
+    const [cocktailData, cocktailDataSet] = useState(null);
+
+    async function getCocktailData(url: string, options?: RequestInit): Promise<any> {
+        try {
+            const response = await fetch(url, options);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            cocktailDataSet(await response.json());
+        } catch (error) {
+            console.error('Fetch error:', error);
+            throw error;
+        }
+    }
+
     return <div className={classes['menu-page']}>
+        {
+            COCKTAIL_NAMES.map(({name, code}) =>{
+                return(
+                    <div key={name} onClick={() => getCocktailData(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=<${code}>`)} className={classes['menu-page-item']}>
+                        <span className={classes['menu-page-text']}>
+                            {name}
+                        </span>
+                    </div>
+                )
+            })
+        }
     </div>
 }
