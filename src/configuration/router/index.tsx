46ed639c70a5731a470008f7f5918ddfbe96c0ce
alaltru1992 <33177697+alaltru1992@users.noreply.github.com>
2025-03-02 21:57:39 +0300

    fixes

--- src/configuration/router/index.tsx
+++ src/configuration/router/index.tsx
@@ -1,17 +1,17 @@
-import React, { Suspense } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { RouteConfig } from '../RouteConfig';
+import  React, {Suspense} from "react";
+import {Route, Routes} from 'react-router-dom'
+import {RouteConfig} from '../RouteConfig'
 
-const Router = () => {
-  return (
-    <Suspense fallback="Loading...">
-      <Routes>
-        {Object.values(RouteConfig).map(({ element, path }) => (
-          <Route path={path} element={element} key={path} />
-        ))}
-      </Routes>
+const Router = () =>{
+    return <Suspense fallback='Loading...'>
+        <Routes>
+            {
+                Object.values(RouteConfig).map(({element, path}) => (
+                    <Route path={path} element={element} key={path}/>
+                ))
+            }
+        </Routes>
     </Suspense>
-  );
-};
+}
 
-export default Router;
+export default Router
