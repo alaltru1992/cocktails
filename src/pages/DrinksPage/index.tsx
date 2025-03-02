bb84157f130f70cc8b77141a9e30330fcf5a3e90
alaltru1992 <33177697+alaltru1992@users.noreply.github.com>
2025-03-02 21:12:58 +0300

    fixes

--- src/pages/DrinksPage/index.tsx
+++ src/pages/DrinksPage/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useLocation } from 'react-router-dom';
 import { useEffect, useMemo } from 'react';
 import {useDrinkName} from 'shared/hooks';
 import { rootState } from 'configuration/store';
